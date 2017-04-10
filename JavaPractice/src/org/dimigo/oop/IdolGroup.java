f6650ce0c66b96535e9b49706eac7fe790f2bac8
kimseongsu <kimseongsu@218.38.137.28>
2017-04-10 09:28:49 +0900

    m

---
+++ JavaPractice/src/org/dimigo/oop/IdolGroup.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    |_IdolGroup.java
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 4. 10.
+ *</pre>
+ *
+ * @author : kimseongsu
+ * @ver : 1.0
+ */
+public class IdolGroup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[ ] groupName = {"빅뱅", "2NE1", "걸스데이"};
+		String[ ][ ] memberName = {
+				{"GD", "태양", "대성", "탑", "승리"},
+				{"CL", "산다라박", "박봄", "민지"},
+				{"유라", "혜리", "소진", "민아"}
+		};
+		
+		for(int i=0 ; i<3 ; i++){
+			System.out.println("<<"+groupName[i]+">>");
+			for(int j=0 ; j<4 ; j++){
+				System.out.println(memberName[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+}
